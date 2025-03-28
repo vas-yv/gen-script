bdad219d1a54659f8ffea247c59df3cb7c03b695
vasuy <vasuyaragorla7@gmail.com>
2025-03-28 17:54:41 +0530

    add

--- 01-gen.sh
+++ 01-gen.sh
@@ -11,7 +11,21 @@ else
 fi
 
 dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "installation of git is failed"
+    exit 1
+ else
+    echo" git installation is success"
+fi       
 
 dnf install mysql -y
+if [ $? -ne 0 ]
+then
+    echo " installation of mysql failed"
+    exit 1
+else
+    echo " installation of mysql success"
+fi        
 
 echo "is proceeding further?"
