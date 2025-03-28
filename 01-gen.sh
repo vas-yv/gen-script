2517c52d2949d5464255854ff12c0b2311f1c862
vasuy <vasuyaragorla7@gmail.com>
2025-03-28 18:13:57 +0530

    add

--- 01-gen.sh
+++ 01-gen.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 userid=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+script_name=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$script_name-$timestamp.log
 
 validate(){
+   
    if [$1 -ne 0 ]
    then
        echo "$2..fail"
@@ -20,9 +24,9 @@ else
     echo "super user"    
 fi
 
-dnf install git -y
+dnf install git -y &>>$logfile
 validate $? "installation of git"
 
 
-dnf install mysql -y
+dnf install mysql -y &>>$logfile
 validate $? "installation of mysql"
